06e0faf6071a1a9b91bb06561fd49c773dce05e4
AnusAhmedkhan <anusahmedkhan519@gmail.com>
2024-06-05 06:01:59 +0500

    push

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const https = require("https");
 const cors = require("cors");
 const vendor = require("./routes/vendorConnectRoutes");
 const port = 5000;
@@ -7,8 +8,21 @@ var corsOptions = {
   origin: "*",
   optionsSuccessStatus: 200,
 };
+const privateKey = fs.readFileSync("./https/privkey.pem", "utf8");
+const certificate = fs.readFileSync("./https/fullchain.pem", "utf8");
+const ca = fs.readFileSync("./https/chain.pem", "utf8");
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
+
 app.use(express.json());
 app.use("/vendor/connect", cors(corsOptions), vendor);
-app.listen(port, () => {
-  console.log("app is running on port " + port);
+// app.listen(port, () => {
+//   console.log("app is running on port " + port);
+// });
+
+https.createServer(credentials, app).listen(port, () => {
+  console.log(`Server is running on Port ${port}`);
 });
